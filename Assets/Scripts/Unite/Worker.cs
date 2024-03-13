fdac0cca269c846cc1455047f7b0f6fa272f43ba
AnuBoss <Anuwat.sura@bumail.net>
2024-03-13 12:57:27 +0700

    Gather

--- Assets/Scripts/Unite/Worker.cs
+++ Assets/Scripts/Unite/Worker.cs
@@ -39,7 +39,15 @@ public class Worker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        switch (unit.State)
+        {
+            case UnitState.MoveToResource:
+                MoveToResourceUpdate();
+                break;
+            case UnitState.Gather:
+                GatherUpdate();
+                break;
+        }
     }
     
     public void ToGatherResource(ResourceSource resource, Vector3 pos)
@@ -58,5 +66,40 @@ public class Worker : MonoBehaviour
         unit.NavAgent.isStopped = false;
         unit.NavAgent.SetDestination(pos);
     }
+    
+    private void MoveToResourceUpdate()
+    {
+        if (Vector3.Distance(transform.position, unit.NavAgent.destination) <= 2f)
+        {
+            if (curResourceSource != null)
+            {
+                unit.LookAt(curResourceSource.transform.position);
+                unit.NavAgent.isStopped = true;
+                unit.SetState(UnitState.Gather);
+            }
+        }
+    }
+    
+    private void GatherUpdate()
+    {
+        if (Time.time - lastGatherTime > gatherRate)
+        {
+            lastGatherTime = Time.time;
+
+            if (amountCarry < maxCarry)
+            {
+                if (curResourceSource != null)
+                {
+                    curResourceSource.GatherResource(gatherAmount);
+
+                    carryType = curResourceSource.RsrcType;
+                    amountCarry += gatherAmount;
+                }
+            }
+            else //amount is full, go back to deliver at HQ
+                unit.SetState(UnitState.DeliverToHQ);
+        }
+    }
+
 
 }
