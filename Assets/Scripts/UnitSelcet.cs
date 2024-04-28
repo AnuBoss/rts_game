8104f432239e29eb12f2845cb4d0d768f5d047a8
AnuBoss <Anuwat.sura@bumail.net>
2024-04-28 17:01:14 +0700

    Resource UI

--- Assets/Scripts/UnitSelcet.cs
+++ Assets/Scripts/UnitSelcet.cs
@@ -78,6 +78,8 @@ public class Command : MonoBehaviour
         if (Input.GetMouseButtonUp(0))
         {
             ReleaseSelectionBox(Input.mousePosition);
+            if (IsPointerOverUIObject())
+                return;
             TrySelect(Input.mousePosition);
         }
 
@@ -293,6 +295,17 @@ public class Command : MonoBehaviour
                 ShowEnemyBuilding(curBuilding);
         }
     }
+
+    //When Touching UI
+    private bool IsPointerOverUIObject()
+    {
+        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+        eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+        return results.Count > 0;
+    }
+
 
 
 
