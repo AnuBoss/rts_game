c810aaee507c27ced429d563c3f1e9c6bc02350b
AnuBoss <Anuwat.sura@bumail.net>
2025-12-08 14:56:20 +0700

    Button map

--- Assets/Scripts/SelectMap.cs
+++ Assets/Scripts/SelectMap.cs
@@ -1,21 +1,38 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI; // เพิ่ม namespace สำหรับ Button
 
 public class SelectMap : MonoBehaviour
 {
     [SerializeField] private string mapScene;
+    [SerializeField] private Button startGameButton;
+
+    private void Start()
+    {
+       
+        if (startGameButton != null)
+        {
+            startGameButton.interactable = false;
+        }
+    }
 
     public void ChooseMap(string mapName)
     {
         mapScene = mapName;
-    }
 
+        
+        if (startGameButton != null && !string.IsNullOrEmpty(mapName))
+        {
+            startGameButton.interactable = true;
+        }
+    }
 
     public void StartGame()
     {
-        if (mapScene == " ")
+        
+        if (string.IsNullOrEmpty(mapScene))
         {
             return;
         }
